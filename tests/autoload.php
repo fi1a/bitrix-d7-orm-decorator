4699fab4aa6e451bc478217276a2a97c798b6721
Nikolay Gorohovsky <fi1a@icloud.com>
2020-10-24 09:40:39 +0500

    autoload

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,20 +1,20 @@
 <?php
 
-if(PHP_SAPI != 'cli'){
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+if (PHP_SAPI !== 'cli') {
     exit('only for cli use');
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$_SERVER['DOCUMENT_ROOT'] = realpath( $_ENV['BITRIX_DIR']);
+$_SERVER['DOCUMENT_ROOT'] = realpath($_ENV['BITRIX_DIR']);
 $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
 
-define('NO_KEEP_STATISTIC', true);
-define('NOT_CHECK_PERMISSIONS',true);
-define('BX_WITH_ON_AFTER_EPILOG', true);
-define('BX_NO_ACCELERATOR_RESET', true);
-
+require_once __DIR__ . '/bxdefine.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
